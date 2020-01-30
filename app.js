764b0bfc1c9a8de3867b6a140ca48ee06c260dc4
Darryl Kilzer <darrylkilzer@gmail.com>
2020-01-30 11:00:21 -0700

    James

--- app.js
+++ app.js
@@ -21,11 +21,18 @@ let orderElem = document.getElementById('order-list')
 
 function orderFood(foodName) {
 
-  orderElem.textContent = (`
+  orderElem.innerHTML = (`
     <h3>You ordered this: ${foods[foodName].name}</h3>
-    
+    <h4> with ingredients: ${foods[foodName].ingredients}</h4>
+    <h4> with spice level: ${foods[foodName].spiciness}</h4>
   `);
 
   console.log(`You are in orderFood ${foods[foodName].name}`);
 
+}
+function drawFood() {
+
+  setTimeout(function () {
+
+  }, 3000);
 }
