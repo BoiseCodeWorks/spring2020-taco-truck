dad8e17d0eeb71ceba463ef3f8a0e91fa8be4f5c
Darryl Kilzer <darrylkilzer@gmail.com>
2020-01-30 11:21:33 -0700

    DevinGray

--- app.js
+++ app.js
@@ -16,23 +16,25 @@ let foods = {
     spiciness: "very"
   }
 }
-
+let food;
 let orderElem = document.getElementById('order-list')
 
 function orderFood(foodName) {
+  food = foods[foodName];
+  orderElem.innerHTML = (`
+    <h3>You ordered this: ${food.name}</h3>
+    <h4> with ingredients: ${food.ingredients}</h4>
+    <h4> with spice level: ${food.spiciness}</h4>
+  `);
 
+  console.log(`You are in orderFood ${foods[foodName].name}`);
+  setTimeout(drawFood, 3000);
+}
+
+function drawFood() {
   orderElem.innerHTML = (`
     <h3>You ordered this: ${foods[foodName].name}</h3>
     <h4> with ingredients: ${foods[foodName].ingredients}</h4>
     <h4> with spice level: ${foods[foodName].spiciness}</h4>
   `);
-
-  console.log(`You are in orderFood ${foods[foodName].name}`);
-
-}
-function drawFood() {
-
-  setTimeout(function () {
-
-  }, 3000);
 }
